0fcac02d5bd51de4133e29dee4166416f7f5c8a3
Ik0l <ikol.MD@gmail.com>
2014-04-09 17:05:43 +0400

    Правка моделей

--- wear/models.py
+++ wear/models.py
@@ -1,9 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class Category(models.Model):
     title = models.CharField(max_length=30)
+
+    class Meta():
+        verbose_name = u'Категория'
+        verbose_name_plural = u'Категории'
 
     def __unicode__(self):
         return u'%s' % (self.title, )
@@ -13,13 +18,16 @@ class Size(models.Model):
     title = models.CharField(max_length=10)
     category = models.ForeignKey(Category)
 
+    class Meta():
+        verbose_name = u'Размер'
+        verbose_name_plural = u'Размеры'
+        ordering = ('title', )
+
     def __unicode__(self):
         return u'%s(%s)' % (self.title, self.category.title)
 
 
 class Cloth(models.Model):
-    class Meta():
-        db_table = 'cloth'
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     price = models.IntegerField()
@@ -33,6 +41,11 @@ class Cloth(models.Model):
     rating_5 = models.IntegerField(default=0)
     sizez = models.ManyToManyField(Size, through='SizeCount')
 
+    class Meta():
+        verbose_name = u'Наименование'
+        verbose_name_plural = u'Наименования'
+        ordering = ('title', )
+
     def __unicode__(self):
         return u'%s' % (self.title)
 
@@ -42,16 +55,27 @@ class SizeCount(models.Model):
     item = models.ForeignKey(Cloth)
     count = models.IntegerField(default=0)
 
+    class Meta():
+        verbose_name = u'Количество размеров'
+        ordering = ('title', )
+
 
 class Gallery(models.Model):
     item = models.ForeignKey(Cloth)
     image = models.ImageField(upload_to="wear/galery", blank=True)
+
+    class Meta():
+        verbose_name = u'Изображение'
+        verbose_name_plural = u'Изображения'
 
 
 class Comments(models.Model):
     item = models.ForeignKey(Cloth)
     text = models.TextField()
 
+    class Meta():
+        verbose_name = u'Комментарий'
+        verbose_name_plural = u'Комментарии'
 
 class Cart(models.Model):
     user = models.ForeignKey(User)
@@ -63,11 +87,18 @@ class Cart(models.Model):
     comments = models.TextField()
     total = models.IntegerField(default=0)
 
+    class Meta():
+        verbose_name = u'Заказ'
+        verbose_name_plural = u'Заказы'
 
 class Contacts(models.Model):
     user = models.ForeignKey(User)
     addres = models.TextField()
     phone = models.CharField(max_length=40)
+
+    class Meta():
+        verbose_name = u'Контактные данные'
+        ordering = ('user', )
 
 
 class Proxy(models.Model):
@@ -75,3 +106,7 @@ class Proxy(models.Model):
     cart = models.ForeignKey(Cart)
     count = models.IntegerField()
     size = models.ForeignKey(Size)
+
+    class Meta():
+        verbose_name = u'Proxy'
+        ordering = ('item', )
