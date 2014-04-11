71bf5fc61ccd729b3f5fcfed96f4881a31e6794f
Ik0l <ikol.MD@gmail.com>
2014-04-11 21:06:34 +0400

    Статика

--- wear/urls.py
+++ wear/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from store import settings
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -14,3 +16,14 @@ urlpatterns = patterns('',
     url(r'^category/(?P<cat>\d+)/$', 'wear.views.wear_list_cat'),
     url(r'^$', 'wear.views.wear_list'),
 )
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root = settings.MEDIA_ROOT,
+    )
+    urlpatterns += staticfiles_urlpatterns()
