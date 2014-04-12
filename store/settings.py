50355e608cc2efa70192c262f6dbd6ceba2270c6
Oxymoron42 <dimka.n42@gmail.com>
2014-04-12 18:41:31 +0400

    почистил код

--- store/settings.py
+++ store/settings.py
@@ -1,28 +1,13 @@
-"""
-Django settings for store project.
+# -*- coding:utf-8 -*-
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 path = lambda *args: os.path.join(BASE_DIR, *args).replace('\\', '/')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '+dy&m(egj0s96l7(6-#4mz9hj)l#7reya(d9*f3z*wgem)d3k*'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
@@ -38,13 +23,14 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 TEMPLATE_DIRS = {
     path('store', 'templates'),
-    path('wear', 'templates'),
+    # нет необходимости прописывать пути к папкам с шаблонами не самого приложения
+    # в во всех модулях по умолчанию ожидается папка templates в корневой папке модуля
+    # сейчас у тебя твоё приложение это store а wear это лишь внешний модуль
+    # path('wear', 'templates'),
 }
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -74,18 +60,12 @@ ROOT_URLCONF = 'store.urls'
 WSGI_APPLICATION = 'store.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'ru-RU'
 
@@ -98,12 +78,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
 STATIC_ROOT = path('www', 'static', )
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = (
     path('store', 'static'),
 )
